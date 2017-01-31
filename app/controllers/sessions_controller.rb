cd8667f3f81528be47a7fd1050da18d828da8f17
Drew Palazzari <drewpalazzari@hotmail.com>
2017-01-30 18:52:19 -0700

    Add authentication PORO

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,7 @@
 class SessionsController < ApplicationController
 
 	def create
-		response 	 = Faraday.post("https://github.com/login/oauth/access_token?client_id=#{ENV["client_id"]}&client_secret=#{ENV["client_secret"]}&code=#{params[:code]}")
-		token 		 = response.body.split(/\W+/)[1]
-		user_data  = Faraday.get("https://api.github.com/user?access_token=#{token}")
-		user_info  = JSON.parse(user_data.body)
-		user 			 = User.from_omniauth(user_info)
-		user.token = token
+		user 	 = Authentication.visit(params[:code])
 		if user.save
 			session[:user_id] = user.id
 			redirect_to dashboard_index_path
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
 
-	def self.from_omniauth(auth_info)
+	def self.from_omniauth(auth_info, token)
 		find_or_create_by(uid: auth_info['id']) do |user|
 			user.uid 	 = auth_info['id']
 			user.name  = auth_info['login']
+			user.token = token
 		end
 	end
 
