619de5ac78670bf164cbb97a6f84eac395bbd668
Drew Palazzari <drewpalazzari@hotmail.com>
2017-01-31 14:46:49 -0700

    Add repository finder on user dashboard

---
+++ app/models/user_repos.rb
@@ -0,0 +1,18 @@
+class UserRepos
+
+	attr_reader :name, :url, :issues, :language
+
+	def initialize(attributes={})
+		@name 		= attributes[:name]
+		@url 			= attributes[:html_url]
+		@issues 	= attributes[:open_issues_count]
+		@language = attributes[:language]
+	end
+
+	def self.find_repos(user)
+		UserReposService.find_repos(user).map do |repo|
+			new(repo)
+		end
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 	get '/auth/github/callback', to: 'sessions#create'
 	get '/logout', to: 'sessions#destroy'
 	get '/search', to: 'search#index'
+	get '/find_repos', to: 'finder#index'
 
 	resources :dashboard, only: [:index]
 end
