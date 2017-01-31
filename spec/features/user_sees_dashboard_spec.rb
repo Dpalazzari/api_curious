f4716f55f044d7fe0ee8d7c37d72f3ff7402c38f
Drew Palazzari <drewpalazzari@hotmail.com>
2017-01-30 19:39:28 -0700

    Add user feature test

--- spec/features/user_sees_dashboard_spec.rb
+++ spec/features/user_sees_dashboard_spec.rb
@@ -1,14 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe 'As a logged in user', type: :feature do
-	it 'user sees welcome message' do
-		user = create(:user)
+RSpec.describe 'Login path', type: :feature do
+	context 'as a logged in user' do
+		it 'user sees welcome message' do
+			user = create(:user)
 
-		page.set_rack_session(user_id: user.id)
+			page.set_rack_session(user_id: user.id)
 
-		visit dashboard_index_path
+			visit dashboard_index_path
 
-		expect(page).to have_content("Hello, #{user.name}")
-		expect(current_path).to eq(dashboard_index_path)
+			expect(page).to have_content("Hello, #{user.name}")
+			expect(page).to have_link("Logout")
+			expect(current_path).to eq(dashboard_index_path)
+		end
+	end
+
+	context 'as a visitor' do
+		it 'visitor sees the login button' do
+			visit root_path
+
+			expect(page).to have_content("Login thru Github")
+		end
 	end
 end
