1ddb7a09dd74e5df21d76091e75083f738d958d6
Drew Palazzari <drewpalazzari@hotmail.com>
2017-02-01 12:57:20 -0700

    Add user service test

--- spec/services/user_repos_service_spec.rb
+++ spec/services/user_repos_service_spec.rb
@@ -1,14 +1,22 @@
 require 'rails_helper'
 
 describe UserReposService do
-  context '.new' do
-    xit 'returns all repos for signed in user' do
+  context '.find_repos' do
+    it 'returns all repos for signed in user' do
       VCR.use_cassette("user_repos_service.new") do
-        {:id=>80458660,
-         :name=>"api_curious",
-         :html_url=>"https://github.com/Dpalazzari/api_curious",
-         :language=>"Ruby",
-         :open_issues=>1}
+        raw_repos = UserReposService.find_repos("Dpalazzari")
+
+        expect(raw_repos).to be_an(Array)
+        expect(raw_repos.count).to eq(22)
+
+        raw_repo = raw_repos.first
+        
+        expect(raw_repo).to be_a(Hash)
+        expect(raw_repo).to have_key(:name)
+        expect(raw_repo).to have_key(:html_url)
+        expect(raw_repo).to have_key(:open_issues_count)
+        expect(raw_repo).to have_key(:language)
+        expect(raw_repo[:name]).to be_a(String)
       end
     end
   end
